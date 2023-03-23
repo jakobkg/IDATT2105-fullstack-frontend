b86941652a3713dd03bbf8f36cd2fb2e0cde70b7
Jakob Grønhaug <jakob@gronha.ug>
2023-03-23 22:51:28 +0100

    Oppsett av innlogging påbegynt

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import { login } from '@/util/login';
+
 export default {
     data() {
         return {
@@ -14,11 +15,56 @@ export default {
 </script>
 
 <template>
-    <div class="login-container">
-        <input type="text" v-model="email">
-        <input type="password" v-model="password">
-        <button @click="() => {login(email, password)}">LOGG INN</button>
-    </div>
+    <main>
+        <div class="login-container">
+            <h2>Logg inn</h2>
+            
+            <div class="field-container">
+                <label for="email">E-post</label>
+                <input type="text" v-model="email">
+            </div>
+            
+            <div class="field-container">
+                <label for="passord">Passord</label>
+                <input type="password" v-model="password">
+            </div>
+            
+            <button @click="() => {login(email, password)}">LOGG INN</button>
+
+            <p><a href="#">Ny bruker</a> - <a href="#">Glemt passord?</a></p>
+        </div>
+    </main>
 </template>
 
-<style></style>
+<style lang="scss">
+$width: 15rem;
+
+main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.login-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    gap: .66rem;
+    padding: 1rem;
+
+    border: 1px solid white;
+    border-radius: 1rem;
+}
+
+.field-container {
+    display: flex;
+    justify-content: space-between;
+    
+    width: $width;
+
+    input {
+        width: 60%;
+    }
+}
+</style>
