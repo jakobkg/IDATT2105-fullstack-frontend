c8765b756702f0c394e88a6338a1795956412bd2
Jakob Grønhaug <jakob@gronha.ug>
2023-03-24 15:07:52 +0100

    Implementer API og store for kategorier

---
+++ src/types/CreateItemRequest.d.ts
@@ -0,0 +1,11 @@
+declare type CreateItemRequest = {
+    title: string;
+    description: string;
+    date: string;
+    latitude: string;
+    longitude: string;
+    price: string;
+    categoryId: number;
+    images: string;
+    userId: number;
+}
