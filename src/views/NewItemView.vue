44615d2571b34bcf13b133289ea107cf8e535edf
Jakob Grønhaug <jakob@gronha.ug>
2023-03-27 11:48:56 +0200

    vent på ekstern API før annonse opprettes

--- src/views/NewItemView.vue
+++ src/views/NewItemView.vue
@@ -1,11 +1,11 @@
 
 <script lang="ts">
 import Album from "@/components/Album.vue";
-import {API} from '@/util/API';
+import { API } from '@/util/API';
 import router from "@/router";
-import {mapState} from "pinia";
-import {useCategoryStore} from "@/store/categoryStore";
-import {useAuthStore} from "@/store/authStore";
+import { mapState } from "pinia";
+import { useCategoryStore } from "@/store/categoryStore";
+import { useAuthStore } from "@/store/authStore";
 
 export default {
   name: "NewItemView",
@@ -14,27 +14,27 @@ export default {
   data() {
     return {
       itemImages: [] as any[],
-      itemText:'',
+      itemText: '',
       itemTitle: "",
-      itemName:"",
+      itemName: "",
       description: "",
       price: "",
       category: "",
       address: "",
-      hei:"",
+      hei: "",
     };
   },
   computed: {
-    ...mapState(useCategoryStore,["categories"]),
+    ...mapState(useCategoryStore, ["categories"]),
     ...mapState(useAuthStore, ['user']),
   },
   methods: {
-    loadImages(){ //Updates the "Album" component and displays the images from in "itemText" field
+    loadImages() { //Updates the "Album" component and displays the images from in "itemText" field
       this.itemImages = this.itemText.split(",").map((itemText: string) => itemText.trim());
       console.log("la inn bilder: ");
       this.itemImages.forEach((item: any) => console.log(item))
     },
-    submit(){
+    submit() {
       const imageList = this.itemText;
 
       //fetches category from select
@@ -42,38 +42,27 @@ export default {
 
       let long: string;
       let lat: string;
+
       //finds coordinates from address
+      const userID = this.user.id;
       const coordinates = API.Location.cityToCoords(this.address)
         .then((coordinates) => {
           long = coordinates.longitude;
           lat = coordinates.latitude;
         })
-
-      const userID = this.user.id;
-
-      API.Loftet.createItem({
-        title: this.itemTitle,
-        description: this.description,
-        price: this.price,
-        latitude: "22",
-        longitude: "33",
-        location: this.address,
-        categoryId: selectedCategoryId,
-        images: imageList,
-        userId: userID,
-        /*
-        title: this.itemTitle,
-        description: this.description,
-        price: this.price,
-        latitude: long,
-        longitude: lat,
-        location: this.address, //todo
-        //date: today,
-        categoryId: selectedCategoryId,
-        images: imageList,
-        userId: userID,
-         */
-      })
+        .then(() => {
+          API.Loftet.createItem({
+            title: this.itemTitle,
+            description: this.description,
+            price: this.price,
+            latitude: long,
+            longitude: lat,
+            location: this.address,
+            categoryId: Number.parseInt(selectedCategoryId),
+            images: imageList,
+            userId: userID,
+          })
+        })
         .then(() => {
           router.push("/");
         })
@@ -88,40 +77,38 @@ export default {
 
 <template>
   <main>
-      <h1>OPPRETT ANNONSE</h1>
+    <h1>OPPRETT ANNONSE</h1>
 
-      <Album :album-images= itemImages />
+    <Album :album-images=itemImages />
 
-      <form @submit.prevent="submit">
-        <label for="title">Annonsetittel:</label><br>
-        <input type="text" v-model="itemTitle" id="title" name="title"  required><br>
+    <form @submit.prevent="submit">
+      <label for="title">Annonsetittel:</label><br>
+      <input type="text" v-model="itemTitle" id="title" name="title" required><br>
 
-        <label for="description">Beskrivelse:</label><br>
-        <textarea id="description" v-model= "description" name="desctiption" required></textarea>
+      <label for="description">Beskrivelse:</label><br>
+      <textarea id="description" v-model="description" name="desctiption" required></textarea>
 
-        <label for="price">Pris:</label><br>
-        <input type="number" id="price" v-model="price" name="price" min="0" required><br>
+      <label for="price">Pris:</label><br>
+      <input type="number" id="price" v-model="price" name="price" min="0" required><br>
 
-        <label for="category">Kategori:</label><br>
-        <select id="category" v-model="category" name="category">
-          <option v-for="category in categories">
-            {{category.id +":" + category.categoryName }}
-          </option>
-        </select><br>
+      <label for="category">Kategori:</label><br>
+      <select id="category" v-model="category" name="category">
+        <option v-for="category in categories">
+          {{ category.id + ":" + category.categoryName }}
+        </option>
+      </select><br>
 
-        <label for="address">Adresse:</label><br>
-        <input type="text" id="address" v-model="address" name="address" required><br>
+      <label for="address">Adresse:</label><br>
+      <input type="text" id="address" v-model="address" name="address" required><br>
 
-        <label for="images">Legg inn komma-separerte bildelenker:</label><br>
-        <input v-model="itemText" type="text" id="images" name="images"><br><br>
-        <button type="button" id="update" @click="loadImages">Oppdater bilder</button>
-        <br><br>
+      <label for="images">Legg inn komma-separerte bildelenker:</label><br>
+      <input v-model="itemText" type="text" id="images" name="images"><br><br>
+      <button type="button" id="update" @click="loadImages">Oppdater bilder</button>
+      <br><br>
 
-        <input type="submit" value="Opprett">
-      </form>
+      <input type="submit" value="Opprett">
+    </form>
   </main>
-
-
 </template>
 
 
@@ -133,23 +120,26 @@ main {
 
 
 
-form{
+form {
   align-content: end;
 }
 
-input[type="text"],input[type="select"],input[type="number"] {
-  width:100%;
+input[type="text"],
+input[type="select"],
+input[type="number"] {
+  width: 100%;
   padding: .5em;
   outline-color: base.$pink;
 }
 
-textarea{
+textarea {
   width: 100%;
   resize: vertical;
   padding: .5em;
 }
 
-input:focus-visible, textarea:focus-visible {
+input:focus-visible,
+textarea:focus-visible {
   outline: 2px solid base.$pink;
   border-radius: 3px;
 }
